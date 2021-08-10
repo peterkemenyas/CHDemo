7882d423aa68aadee4237d11685148ece94b1774
peterkemenyas <peter.kemenyas@protonmail.ch>
2021-08-11 00:59:05 +0200

    Added Login form validation test cases

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -2,12 +2,12 @@
 import LoginPage from '../support/pageObjects/LoginPage'
 
 
-describe('Login TS', ()=>{
+/*describe('Login TestSuite', ()=>{
 
     let username;
     let password;
 
-    it('Login valid password valid name', () => {
+    it('Login - correct username and correct password', () => {
         username = 'peter.kemenyas@protonmail.ch';
         password = 'PasCra194613';
 
@@ -16,12 +16,102 @@ describe('Login TS', ()=>{
         coOwnership.verifyProfileName('P. Kemenyas');
         
         /*
+        Example of non Page Object test implementation
         cy.visit('/login')
         cy.get('[data-testid="email"]').type('peter.kemenyas@protonmail.ch')
         cy.get('[data-testid="password"]').type('PasCra194613')
         cy.get('button[type="submit"]').contains('Einloggen').click()
         cy.get('[data-testid="header-content"]').contains('P. Kemenyas').should('be.visible')
-        */
+        
+    })
 
+    it('Login - incorrect username and correct password', () => {
+        username = 'peter.WRONG@protonmail.ch';
+        password = 'PasCra194613';
+
+        const loginPage = new LoginPage();
+        loginPage.login(username, password);
+        loginPage.verifyErrorMessage('Die eingegebene E-Mail-Adresse oder das Passwort ist falsch.');
+
+    })
+
+    it('Login - correct username and incorrect password', () => {
+        username = 'peter.kemenyas@protonmail.ch';
+        password = 'PasCraWRONG';
+
+        const loginPage = new LoginPage();
+        loginPage.login(username, password);
+        loginPage.verifyErrorMessage('Die eingegebene E-Mail-Adresse oder das Passwort ist falsch.');
+
+    })
+
+    it('Login - icorrect username and incorrect password', () => {
+        username = 'peter.WRONG@protonmail.ch';
+        password = 'PasCraWRONG';
+
+        const loginPage = new LoginPage();
+        loginPage.login(username, password);
+        loginPage.verifyErrorMessage('Die eingegebene E-Mail-Adresse oder das Passwort ist falsch.');
+
+    })
+})*/
+
+describe('Login form validation TestSuite', () => {
+    let username
+    let password
+
+    it('Valid username',()=>{
+        username = 'valid.format@valid.format';
+
+        const loginPage = new LoginPage();
+        loginPage.visit();
+        loginPage.fillEmail(username);
+        loginPage.verifyEmailValidationMsg('Bitte geben Sie eine gültige E-Mail-Adresse ein.', false);
+        
+    })
+
+    it('Invalid username',()=>{
+        username = 'invalid.format@invalid';
+
+        const loginPage = new LoginPage();
+        loginPage.visit();
+        loginPage.fillEmail(username);
+        loginPage.unfocusEmail();
+        loginPage.verifyEmailValidationMsg('Bitte geben Sie eine gültige E-Mail-Adresse ein.', true);
+        
+    })
+
+    it('Valid password',()=>{
+        password = '003456789dfg';
+
+        const loginPage = new LoginPage();
+        loginPage.visit();
+        loginPage.fillPassword(password);
+        loginPage.unfocusPassword();
+        loginPage.verifyPasswordValidationMsg('Ihr Passwort muss mindestens 6 Zeichen lang sein.', false);
+        
+    })
+
+    it('Empty password',()=>{
+        password = '';
+        username = 'valid.format@valid.format';
+
+        const loginPage = new LoginPage();
+        loginPage.visit();
+        loginPage.fillEmail(username)
+        loginPage.fillPassword(password);
+        loginPage.unfocusPassword();
+        loginPage.verifyPasswordValidationMsg('Ihr Passwort muss mindestens 6 Zeichen lang sein.', true);
+        
+    })
+
+    it('Empty email',()=>{
+        username = '';
+
+        const loginPage = new LoginPage();
+        loginPage.visit();
+        loginPage.fillEmail(username)
+        loginPage.unfocusEmail();
+        loginPage.verifyEmailValidationMsg('Bitte geben Sie eine gültige E-Mail-Adresse ein.', true);
     })
 })
--- cypress/support/pageObjects/LoginPage.js
+++ cypress/support/pageObjects/LoginPage.js
@@ -7,9 +7,13 @@ class LoginPage{
     constructor(){}
 
     loginUrl = '/login';
+    emailInp = '[name="email"][type="text"]';
     emailTxt = '[data-testid="email"]';
+    PasswordInp = '[name="password"][type="password"]';
     passwordTxt = '[data-testid="password"]';
     loginBtn = 'button[type="submit"]';
+    errorMsgPnl = 'div.Login_alert__1oxaM';
+    errorMsg = 'div.MuiAlert-message'
 
     visit(){
         cy.visit(this.loginUrl);
@@ -21,7 +25,7 @@ class LoginPage{
         if(username==''){
             return this;
         } else {
-            field.type(username, {delay: 100});
+            field.type(username);
         }
         return this;
     }
@@ -30,15 +34,27 @@ class LoginPage{
         const field = cy.get(this.passwordTxt);
         field.clear;
         if(password == ''){
-            return this;
+            cy.get(this.PasswordInp).focus();
         } else {
-            field.type(password, {delay:100});
+            field.type(password);
         }
+        return this;
     }
 
     clickSubmit(){
         const button = cy.get(this.loginBtn);
         button.click();
+    }
+
+    unfocusEmail(){
+        const field = cy.get(this.PasswordInp);
+        field.focus(); 
+    }
+
+    unfocusPassword(){
+        const field = cy.get(this.emailInp);
+        field.focus(); 
+
     }
 
     login(username, password){
@@ -47,6 +63,36 @@ class LoginPage{
         this.fillPassword(password);
         this.clickSubmit();
         return new CoOwnership();
+    }
+
+    verifyErrorMessage(message){
+        cy.get(this.errorMsgPnl).within(()=>{
+            cy.get(this.errorMsg).should('have.text', message);
+        })
+    }
+
+    verifyEmailValidationMsg(message, isDisplayed){
+        if(isDisplayed){
+            cy.get(this.emailTxt).within(()=>{
+                cy.contains(message).should('be.visible')
+            })       
+        } else {
+            cy.get(this.emailTxt).within(()=>{
+                cy.contains(message).should('not.exist')
+            })
+        }
+    }
+
+    verifyPasswordValidationMsg(message, isDisplayed){
+        if(isDisplayed){
+            cy.get(this.passwordTxt).within(()=>{
+                cy.contains(message).should('be.visible')
+            })       
+        } else {
+            cy.get(this.passwordTxt).within(()=>{
+                cy.contains(message).should('not.exist')
+            })
+        }
     }
 
 }
